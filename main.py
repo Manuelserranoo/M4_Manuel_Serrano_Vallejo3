b297f7bbc226ff2a627dff1883ec831801188cff
Manuelserranoo <113980842+Manuelserranoo@users.noreply.github.com>
2022-12-10 12:10:49 +0000

    cambios

--- main.py
+++ main.py
@@ -32,73 +32,73 @@ rep = titulos["title"].value_counts()
 print(rep)
 
 #Como solo necesitamos los 10 más repetidos pedimos que imprima los 10 primeros con .head(10).
-rep.head(10)
+print(rep.head(10))
 
 #Como nos piden la pélicula llamada "Romeo and Juliet" más antigua primero aislamos todas las llamadas así buscando en titulos.title las que tienen ese nombre.
 ryj = titulos[titulos.title == "Romeo and Juliet"]
-ryj
+print(ryj)
 
 #Como necesitamos la más pequeña utilizamos al igual que anteriormente nsmallest en la columna de "year".
-ryj.nsmallest(1, ["year"])
+print(ryj.nsmallest(1, ["year"]))
 
 #Como necesitamos las peliculas que contengan la palabra "Exorcist" no nos vale con el método del ejercicio anterior por lo que buscamos dentro de title mediante .str.contains("Exorcist) los títulos que contengan esta palabra.
 exo = titulos[titulos["title"].str.contains("Exorcist")]
-exo
+print(exo)
 
 #Los ordenamos de menos a mayor con .sort_values.
-exo.sort_values("year")
+print(exo.sort_values("year"))
 
 #Aislamos los registros del año 1950.
 pl = titulos[titulos["year"] == 1950]
-pl
+print(pl)
 
 #Mediante len() calculamos la longitud de la nueva lista.
-len(pl)
+print(len(pl))
 
 #Para tener un rango de años primero nos centramos en aquellos > o = a 1950 y luego en la nueva lista nos centramos en los menores o iguales a 1959.
 d = titulos[titulos["year"] >= 1950 ]
 f = d[titulos["year"] <= 1959]
-f
+print(f)
 
 #Buscamos en el apartado "title" de elenco "The Godfather"
 e = elenco[elenco.title == "The Godfather"]
-e
+print(e)
 
 #Mediante e.iloc [:,4] nos quedamos solo con la columna que buscamos.
 c = e.iloc [:,4]
-c
+print(c)
 
 #Para ver que valores se repiten utilizamos en la nueva lista c la función value_counts().
 papeles = c.value_counts()
-papeles
+print(papeles)
 
 #Buscamos las peliculas que se llamen "Dracula".
 drac = elenco[elenco.title == "Dracula"]
-drac
+print(drac)
 
 #Para ordenarlas por el valor n ordenamos que la lista nueva se ordene de menos a mayor con .nsmallest.
-drac.nsmallest(113, ["n"])
+print(drac.nsmallest(113, ["n"]))
 
 #Buscamos el valor "Bruce Wayne" en la columna character.
 b = elenco[elenco.character == "Bruce Wayne"]
-b
+print(b)
 
 #Medimos la longitud de la lista con len().
-len(b)
+print(len(b))
 
 #Seguimos la misma técnica que en anterior apartado.
 r = elenco[elenco.name == "Robert De Niro"]
-r
-len(r)
+print(r)
+print(len(r))
 
 #En este caso necesitamos encadenar varias funciones para conseguir la lista con requisitos que nos piden.
 s = elenco[elenco.name == "Charlton Heston"]
 z = s[elenco.n == 1]
 k = z[elenco["year"] >= 1950 ]
-k[elenco["year"] <= 1959 ]
+print(k[elenco["year"] <= 1959 ])
 
 #En este apartado nos quedamos con la lista solamente de los años 50´s y luego medimos la longitud en el caso de que sean actores y en el caso de que sean actrices.
 w = elenco[elenco["year"] <= 1949]
 y = w[elenco["year"] >= 1940]
-len(w[elenco.type == "actor"])
-len(w[elenco.type == "actress"])
+print(len(w[elenco.type == "actor"]))
+print(len(w[elenco.type == "actress"]))
